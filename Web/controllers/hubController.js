19951423e4f06cc4632bc4216867e3af6f19bd7c
GDemay <guillaume.demay@epitech.eu>
2018-03-15 00:11:30 +0100

    Upload dans BDD

--- Web/controllers/hubController.js
+++ Web/controllers/hubController.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var Photo = require('../models/hub');
+var Hub = require('../models/hub');
 
 exports.display = function(req, res)
 {
@@ -8,7 +8,47 @@ exports.display = function(req, res)
 
 exports.upload = function(req, res)
 {
-  console.log(req.files);
   res.render('hub', {title: 'Upload'});
 
+  var datapicture = {
+    originalname: req.files[0].originalname,
+    mimetype: req.files[0].mimetype,
+    destination: req.files[0].destination,
+    filename: req.files[0].filename,
+    path: req.files[0].path,
+    size: req.files[0].size,
+  }
+  //res.send(req.files.map);
+  console.log(datapicture);
+
+
+  var HubSchm = new mongoose.Schema({
+    originalname : String,
+    mimetype : String,
+    destination : String,
+    filename : String,
+    path : String,
+    size : String,
+    date : { type : Date, default : Date.now }
+  });
+
+  // Création du Model pour les commentaires
+  var HubModel = mongoose.model('commentaires', HubSchm);
+
+  // On crée une instance du Model
+  var instModel = new HubModel();
+  instModel.originalname = req.files[0].originalname;
+  instModel.mimetype = req.files[0].mimetype;
+  instModel.destination = req.files[0].destination;
+  instModel.filename = req.files[0].filename;
+  instModel.path = req.files[0].path;
+  instModel.size = req.files[0].size;
+
+
+  instModel.save(function (err) {
+    if (err) {
+      res.send("ERROR UPLOAD");
+      throw err; }
+    console.log('Instance ajoutée avec succès !');
+  });
 };
--- Web/models/hub.js
+++ Web/models/hub.js
@@ -1,17 +1,15 @@
 var mongoose = require('mongoose');
 var HubSchema = new mongoose.Schema({
-    nom: {
-	type: String,
-	unique: true,
-	required: true,
-	trim: true
-    },
-    date: {
-	type: Date,
-	unique: true,
-	required: true,
-	trim: true
-    }
+  originalname : String,
+  mimetype : String,
+  destination : String,
+  filename : String,
+  path : String,
+  size : String,
+  date : { type : Date, default : Date.now }
 });
+
+
+
 var Hub = mongoose.model('Hub', HubSchema);
 module.exports = Hub;
