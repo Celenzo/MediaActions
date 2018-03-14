559001322dcf70321f8f5599da2e6e86d3063aaf
GDemay <guillaume.demay@epitech.eu>
2018-03-14 22:01:26 +0100

    push

--- Web/controllers/hubController.js
+++ Web/controllers/hubController.js
@@ -8,33 +8,8 @@ exports.display = function(req, res)
 
 exports.upload = function(req, res)
 {
-  res.send(req.files);
-};
-
-
-
-
-    //
-
-/*
-
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
-*/
-/*
-exports.display = function(req, res, next) {
+  console.log("bite");
+  console.log(req.files);
+  res.render('hub', {title: 'Upload'});
 
 };
-
-*/
--- Web/routes/hub.js
+++ Web/routes/hub.js
@@ -5,14 +5,8 @@ var upload = multer({dest: 'public/uploads/'})
 var hub_controller = require('../controllers/hubController');
 
 /* GET home page. */
-router.get('/hub', upload.any(), hub_controller.display);
-router.post('/hub', upload.any(), hub_controller.display);
+router.get('/hub', upload.single('avatar'), hub_controller.display);
+router.post('/hub', upload.single('avatar'), hub_controller.upload);
 
-
-
-
-
-
-//router.post('/hub', hub_controller.display);
 
 module.exports = router;
