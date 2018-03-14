92cded8b2bbf75814c137d72535b51012842f12d
celenzo crespin <celenzo.crespin@epitech.eu>
2018-03-14 11:56:00 +0100

    theme change

--- Android/ma_androidapp/app/src/main/java/com/mediaactions/ma_androidapp/MainActivity.java
+++ Android/ma_androidapp/app/src/main/java/com/mediaactions/ma_androidapp/MainActivity.java
@@ -10,4 +10,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void loginBtnFn() {
+
+    }
+
+    protected void registerBtnFn() {
+
+    }
 }
