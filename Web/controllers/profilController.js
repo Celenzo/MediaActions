80f0e75408c71a331f856dd33bc1868f2b09cede
henry_k <benjamin.henry@epitech.eu>
2018-03-21 13:25:57 +0100

    admin

--- Web/controllers/profilController.js
+++ Web/controllers/profilController.js
@@ -1,18 +1,21 @@
 var mongoose = require('mongoose');
 var User     = require('../models/users')
+var nm       = "toto"
+var name     = User.username
 
 const keyPublishable = process.env.PUBLISHABLE_KEY;
 const keySecret = process.env.SECRET_KEY;
 
-function funkytown()
-{
-    User.find({}, {}).exec(function(err, Result){
-	console.log(Result);
+exports.profil = function(req, res, next) {
+    nm = funkytown(function (Result) {
+	res.render('profil', {name});
     })
 }
 
-exports.profil = function(req, res, next) {
-    funkytown();
-    res.render('profil');
+function funkytown(callback, name)
+{
+    User.find({}, {}).exec(function(err, Result){
+	callback(Result[0]["username"]);
+    })
+}
 
-};
